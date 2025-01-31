7285eceda3fa9440fb042ff7f6daffb9f34ab1bf
Ronan Pelliard <ronan.pelliard@datadoghq.com>
2025-01-31 14:27:17 +0100

    Update create-oauth-app.sh

--- create-oauth-app.sh
+++ create-oauth-app.sh
@@ -51,7 +51,7 @@ if ! DD_RESP=`curl -sS --fail-with-body -X POST https://api.$DD_SITE/api/v2/sour
 			\"hostname\": \"$GITLAB_HOSTNAME\",
 			\"client_id\": \"$CLIENT_ID\",
 			\"client_secret\": \"$CLIENT_SECRET\",
-			\"scopes\": [\"api\", \"read_api\", \"read_repository\", \"write_repository\"]
+			\"scopes\": [\"api\", \"read_api\", \"read_user\", \"read_repository\", \"write_repository\"]
 		}
 	}
 }"` ; then
