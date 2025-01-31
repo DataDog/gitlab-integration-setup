d6eaa1eecafbaf544de4e64b67747d4e5bac9b16
Ronan Pelliard <ronan.pelliard@datadoghq.com>
2025-01-31 09:41:01 +0100

    Update create-oauth-app.sh

--- create-oauth-app.sh
+++ create-oauth-app.sh
@@ -26,7 +26,7 @@ if ! APP_RESP=`curl -sS --fail-with-body -X POST https://$GITLAB_HOSTNAME/api/v4
   -d "{
     \"name\": \"Datadog\",
     \"redirect_uri\": \"https://$DD_DOMAIN/api/ui/integration/gitlab/oauth/callback\",
-    \"scopes\": \"api read_api read_repository write_repository\"
+    \"scopes\": \"api read_api read_user read_repository write_repository\"
   }"` ; then
     echo "Failed to create OAuth application in GitLab: $APP_RESP"
     exit 1
